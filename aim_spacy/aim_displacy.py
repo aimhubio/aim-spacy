fa5632fd5a1046a6724b910b677dddf60e6bf8dc
Ceyda Cinarel <15624271+cceyda@users.noreply.github.com>
2022-11-23 20:38:16 +0900

    add option to use with dictionaries  (#3)
    * add option to pass manual entities(dict formatted)
    
    * pass manual at init

--- aim_spacy/aim_displacy.py
+++ aim_spacy/aim_displacy.py
@@ -7,9 +7,10 @@ from aim_spacy.utils import svg_to_png, html_to_img
 
 
 class AimDisplaCy:
-    def __init__(self, image_size=(200, 200), **kwargs):
+    def __init__(self, image_size=(200, 200), manual=False, **kwargs):
         self.image_size = image_size
         self.options = kwargs
+        self.manual = manual
 
 
     def __call__(self, docs, style, caption='', quality=90, optimize=False):
@@ -17,8 +18,9 @@ class AimDisplaCy:
         image_list = []
 
         self.options.update(dict(jupyter=False, style=style, page=False))
+        
         for doc in docs:
-            html = displacy.render(doc, jupyter=False, style=style, page=False, options=self.options)
+            html = displacy.render(doc, jupyter=False, style=style, page=False, manual=self.manual, options=self.options)
 
             if style == 'dep':
                 img = svg_to_png(html)
